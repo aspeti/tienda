3eea2614439edb4cc1d0ae101ed8c8930b248019
Rodrigo Aspeti <rodrigo.aspeti@meta-dao.com>
2023-09-28 15:10:22 -0400

    remove profile option in asside

--- application/views/layouts/aside.php
+++ application/views/layouts/aside.php
@@ -1,9 +1,9 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="<?php echo base_url();?>assets/img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">TIENDA</span>
+    <a href="#" class="brand-link">
+      <img src="<?php echo base_url();?>assets/img/logo.png" class="brand-image elevation-0" style="opacity: .8">
+      <span class="brand-text font-weight-light"><strong>Multitud Deportiva</strong></span>
     </a>
 
     <!-- Sidebar -->
@@ -54,33 +54,7 @@
                       </li>   
             </ul>
           </li>           
-          <?php }?>     
-          <li class="nav-item">
-            <a href="#" class="nav-link">                           
-              <i class="nav-icon fas fa-cog"></i>           
-              <p>
-                Profile
-                <i class="fas fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="<?php echo base_url()?>usuarios/viewprofile/<?php echo $this->session->userdata('id_usuario'); ?> " class="nav-link">
-                  <i class="fas fa-user nav-icon"></i>
-                  <p>Perfil</p>
-                </a>
-              </li>   
-            </ul>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-              <a href="<?php echo base_url('usuarios/password/'.$this->session->userdata('id_usuario')); ?>" class="nav-link">
-                  <i class="fas fa-user nav-icon"></i>
-                  <p>Password</p>
-                </a>
-              </li>   
-            </ul>           
-          </li> 
-                   
+          <?php }?>                        
           <?php if($this->session->userdata('rol') == 2) { ?>   
                 
                   <li class="nav-item">
